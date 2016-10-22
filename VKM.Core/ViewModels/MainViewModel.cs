1ead24265e7f422fc1418e01b29dedbcbfff6eba
i-ka <i-ka2010@mail.ru>
2016-10-22 15:34:45 +0600

    MainViewModel: added OptionsButtonCommand

--- VKM.Core/ViewModels/MainViewModel.cs
+++ VKM.Core/ViewModels/MainViewModel.cs
@@ -10,6 +10,21 @@ namespace VKM.Core.ViewModels
     class MainViewModel :
         MvxViewModel
     {
+        private MvxCommand _optionsButtonCommand;
+        public MvxCommand OptionsButtonCommand
+        {
+            get
+            {
+                if (_optionsButtonCommand == null) {
+                    _optionsButtonCommand = new MvxCommand(OnOptionsButtonClicked);
+                }
+                return _optionsButtonCommand;
+            }
+        }
 
+        private void OnOptionsButtonClicked()
+        {
+            ShowViewModel<OptionsViewModel>();
+        }
     }
 }
