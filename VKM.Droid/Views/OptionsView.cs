079da1809ce600412429054a09c6c9b5376d32f7
i-ka <i-ka2010@mail.ru>
2016-10-22 15:34:07 +0600

    Added OptionsView

---
+++ VKM.Droid/Views/OptionsView.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using MvvmCross.Droid.Views;
+
+namespace VKM.Droid.Views
+{
+    [Activity(Label = "Options view",
+        Theme = "@android:style/Theme.Holo")]
+    class OptionsView : MvxActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.OptionsView);
+        }
+    }
+}
