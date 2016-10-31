0eb2421435a82879010d9e62b7989769d743e13e
i-ka <i-ka2010@mail.ru>
2016-10-31 10:12:14 +0500

    MainViewModel: Get dummy audio list from VkAudioService

--- VKM.Core/ViewModels/MainViewModel.cs
+++ VKM.Core/ViewModels/MainViewModel.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MvvmCross.Core.ViewModels;
+using VKM.Core.Services;
+
 
 namespace VKM.Core.ViewModels
 {
     class MainViewModel :
         MvxViewModel
     {
+        public MainViewModel(IVkAudioService service)
+        {
+            AudioList = service.GetAudioList();
+        }
+        private List<Audio> _audioList;
+        public List<Audio> AudioList
+        {
+            get { return _audioList; }
+            set
+            {
+                _audioList = value;
+                RaisePropertyChanged(() => AudioList);
+            }
+        }
+
         private MvxCommand _optionsButtonCommand;
         public MvxCommand OptionsButtonCommand
         {
