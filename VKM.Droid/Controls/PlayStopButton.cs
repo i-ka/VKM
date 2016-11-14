fbddeaabfb05c34c316e9ba95cc91cbf399896c3
i-ka <i-ka2010@mail.ru>
2016-11-14 13:09:48 +0500

    Added play stop button control

---
+++ VKM.Droid/Controls/PlayStopButton.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace VKM.Droid.Controls
+{
+    class PlayStopButton : ImageView
+    {
+        public PlayStopButton(Context context) : base(context)
+        {
+        }
+
+        public PlayStopButton(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+        }
+
+        public PlayStopButton(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+        {
+        }
+
+        public PlayStopButton(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes)
+        {
+        }
+
+        protected PlayStopButton(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+        private bool _isPlaying = false;
+        public bool IsPlaying
+        {
+            get { return _isPlaying; }
+            set
+            {
+                _isPlaying = value;
+                if (_isPlaying)
+                {
+                    SetImageResource(Resource.Mipmap.ic_action_magnifier);
+                }
+                else
+                {
+                    SetImageResource(Resource.Mipmap.ic_play_button);
+                }
+            }
+        }
+    }
+}
